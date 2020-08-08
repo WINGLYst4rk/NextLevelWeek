1d847f35c261324879c1258e2b832aad7f5ed155
WINGLYst4rk <wingly205@gmail.com>
2020-08-08 00:37:23 -0300

    Back-end logic and adding dynamism on the page

--- src/server.js
+++ src/server.js
@@ -11,44 +11,77 @@ const proffys = [
     },
     {name: "Leticia Martins", 
     avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-    whatsapp: "985234567", 
+    whatsapp: "63984367573", 
     bio: "Entusiasta das melhores tecnologias de química avançada.<br/><br/>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.", 
-    subject: "Química", 
-    cost: "20",
-     weekday: [0],
-     time_from: [720],
+    subject: "Matemática", 
+    cost: "40",
+     weekday: [1],
+     time_from: [1440],
      time_to: [1220]
     }
 ]
-// const express = require('express'); /* um dia vou usar esse estilo */
+
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
 
 function pageLanding(req, res) {
-    return res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
 }
 
 function pageStudy(req, res) {
-    return res.sendFile(__dirname + "/views/study.html")
+    const filters = req.query
+    return res.render("study.html", {proffys, filters, subjects, weekdays})
+    
 }
 
 function pageGiveClasses(req, res) {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+    const data = req.query
+    const isNotEmpty = object.keys(data).length > 0
+
+    if (isNotEmpty){
+        proffys.push(data)
+        return res.redirect("/study")
+    }
+    return res.render("give-classes.html", {subjects, weekdays})
 }
 
 //importação de dependências
+// const express = require('express'); /* um dia vou usar esse estilo */
 const express = require('express');//finalmente usei
 const server = express();
 const nunjucks = require('nunjucks');
 
-//configuração de arquivos estáticos
-server.use(express.static("public"))
 
 //configuração do nunjucks
 nunjucks.configure("src/views", {
     express: server,
     noCache: true,
-    
+
 })
 
+
+//configuração de arquivos estáticos
+server.use(express.static("public"))
 //rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
